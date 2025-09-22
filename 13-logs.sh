78a664cf660a1ce73c313a180f47e42e71433a1e
dheera82s <610anilkumarb@gmail.com>
2025-09-22 15:54:36 +0530

    shell-script

---
+++ 13-logs.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%y-%m-%d-%h-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 ... $R FAILURE"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS"
+    fi  
+}
+
+echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: you must have access to execute this script"
+    exit 1 #other than 0
+fi  
+   
+dnf list installed mysql &>>$LOG_FILE 
+
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y &>>$LOG_FILE
+    VALIDATE $? "INSTALLING MYSQL"   
+else
+    echo -e "mysql is already ... $Y INSTALLED"
+fi               
+
+       
+dnf list installed git &>>$LOG_FILE
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y &>>$LOG_FILE
+    VALIDATE $? "INSTALLING Git"
+else 
+    echo -e "git is already ... $Y INSTALLED"
+fi  
