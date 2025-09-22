129bec64aa3c289574658a6046586f1bbe06e9eb
dheera82s <610anilkumarb@gmail.com>
2025-09-22 15:57:17 +0530

    shell-script

--- 13-logs.sh
+++ 13-logs.sh
@@ -19,30 +19,29 @@ VALIDATE(){
     fi  
 }
 
-echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE
-
+echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: you must have access to execute this script"
     exit 1 #other than 0
 fi  
    
-dnf list installed mysql &>>$LOG_FILE 
+dnf list installed mysql &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then # not installed
-    dnf install mysql -y &>>$LOG_FILE
+    dnf install mysql -y &>>$LOG_FILE_NAME
     VALIDATE $? "INSTALLING MYSQL"   
 else
     echo -e "mysql is already ... $Y INSTALLED"
 fi               
 
        
-dnf list installed git &>>$LOG_FILE
+dnf list installed git &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then
-    dnf install git -y &>>$LOG_FILE
+    dnf install git -y &>>$LOG_FILE_NAME
     VALIDATE $? "INSTALLING Git"
 else 
     echo -e "git is already ... $Y INSTALLED"
